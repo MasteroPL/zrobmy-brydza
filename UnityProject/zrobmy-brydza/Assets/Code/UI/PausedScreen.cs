c35e0881ef2263ce6e4ec34c344d3761e5baca95
marcin8899 <marcinbaranowski8899@gmail.com>
2020-11-01 19:55:53 +0100

    Ekrany pauzy i czekania, na razie oddzielnie

---
+++ UnityProject/zrobmy-brydza/Assets/Code/UI/PausedScreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PausedScreen : MonoBehaviour {
+   [SerializeField] TextMeshProUGUI nicknameText ;
+
+   
+   void setText(string nickname) {
+       nicknameText.text = nickname;
+   }
+}
---
+++ UnityProject/zrobmy-brydza/Assets/Code/UI/WaitingScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WaitingScreen : MonoBehaviour {
+   [SerializeField] TextMeshProUGUI numberOfMissingPlayersText ;
+   int numberOfMissingPlayers;
+
+   void Start () {
+       numberOfMissingPlayers = 3;
+       setText();
+   }
+
+
+   void increaseNumberOfMissingPlayers() {
+      if (numberOfMissingPlayers < 3) {
+         numberOfMissingPlayers = numberOfMissingPlayers + 1;
+         setText();
+      }
+   }
+   void decreaseNumberOfMissingPlayers() {
+      if (numberOfMissingPlayers > 0) {
+         numberOfMissingPlayers = numberOfMissingPlayers -1;
+         setText();
+      }
+   }
+   void setText() {
+       numberOfMissingPlayersText.text = numberOfMissingPlayers.ToString();
+   }
+}
