30be4f66a2dc5cb3a026b10abf45c4bb7b5598eb
niedzwir <r_adekn@wp.pl>
2020-11-01 18:26:30 +0100

    scena gameplay, podstawa funkcji UI rozgrywki

---
+++ UnityProject/zrobmy-brydza/Assets/Code/UI/PlayerDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerDisplay : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI Name;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		Name.text = "wolne miejsce";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityProject/zrobmy-brydza/Assets/Code/UI/Stats.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Stats : MonoBehaviour
+{
+	[SerializeField] int State;
+	[SerializeField] string Chat;
+	[SerializeField] string Points;
+	[SerializeField] string Bets;
+	[SerializeField] TextMeshProUGUI Display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(State == 0){
+			this.Display.text = this.Chat;
+		}
+		else if (State == 1){
+			this.Display.text = this.Points;
+		}
+		else if (State == 2){
+			this.Display.text = this.Bets;
+		}
+		else{
+			this.Display.text = this.Chat;
+		}
+    }
+	
+	public void DisplayChat(){
+		this.State = 0;
+	}
+	
+	public void DisplayPoints(){
+		this.State = 1;
+	}
+	
+	public void DisplayBets(){
+		this.State = 2;
+	}
+	
+}
