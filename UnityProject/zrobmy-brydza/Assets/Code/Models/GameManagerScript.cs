7d5073b793e3d7ecd2d1dcde8defc7a871683e59
jan@sharedmasters.com <jan.przychodniak@gmail.com>
2020-11-02 09:36:59 +0100

    Bazowa struktura danych gry

--- UnityProject/zrobmy-brydza/Assets/Code/Models/GameManagerScript.cs
+++ UnityProject/zrobmy-brydza/Assets/Code/Models/GameManagerScript.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-
-
+    private GameState GameState = GameState.AWAITING_PLAYERS;
+    private PlayerTag CurrentPlayer = PlayerTag.NOBODY;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +23,18 @@ public class GameManagerScript : MonoBehaviour
 public enum GameState
 {
     AWAITING_PLAYERS = 0,
-    STARTING = 1
+    STARTING = 1,
+    BIDDING = 2,
+    PLAYING = 3,
+    PAUSED = 4,
+    GAME_FINISHED = 5
+}
 
+public enum PlayerTag
+{
+    NOBODY = -1,
+    N = 0,
+    E = 1,
+    S = 2,
+    W = 3
 }
