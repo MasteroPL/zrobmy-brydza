9d744b1fca57790dd80f5e32f6a4958d0997f351
jan@sharedmasters.com <jan.przychodniak@gmail.com>
2020-11-02 09:25:32 +0100

    reversing

---
+++ UnityProject/zrobmy-brydza/Assets/Code/Models/GameManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum GameState
+{
+    AWAITING_PLAYERS = 0,
+    STARTING = 1
+
+}
