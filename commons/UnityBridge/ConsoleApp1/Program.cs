b366535d9482b82579e38c4b38c888a0b0885578
El_Padrino <adrian.ukryte@gmail.com>
2020-11-10 23:17:26 +0100

    changes to shuffling algorithm

--- commons/UnityBridge/ConsoleApp1/Program.cs
+++ commons/UnityBridge/ConsoleApp1/Program.cs
@@ -7,27 +7,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            CardShuffler cardShuffler = new CardShuffler();
-            List<Card> deck = cardShuffler.GenerateDeck();
+            List<Card> deck = GenerateDeck();
+            CardShuffler<Card> cardShuffler = new CardShuffler<Card>(deck);
 
             double[] playersPoints = { 0, 0, 0, 0 };
 
             int maxIt = 5;
             for(int it = 0; it < maxIt; it++)
             {
-                deck = cardShuffler.Shuffle(deck);
+                deck = cardShuffler.Shuffle();
 
                 List<List<Card>> decks = cardShuffler.GiveCardsToPlayers(deck);
                 Console.WriteLine("Game {0}/{1}", it + 1, maxIt);
                 for (int i = 0; i < decks.Count; i++)
                 {
-                    playersPoints[i] += (double)cardShuffler.CalculatePoints(decks[i]) / maxIt;
+                    playersPoints[i] += (double)CalculatePoints(decks[i]) / maxIt;
                     /*foreach(Card card in decks[i])
                     {
                         Console.WriteLine(card.ToString());
                     }
                     Console.WriteLine();*/
-                    Console.WriteLine("Player {0} => points : {1}", i + 1, cardShuffler.CalculatePoints(decks[i]));
+                    Console.WriteLine("Player {0} => points : {1}", i + 1, CalculatePoints(decks[i]));
                 }
 
                 Console.WriteLine("");
@@ -39,6 +39,68 @@ namespace ConsoleApp1
                 Console.WriteLine( "Player {0}: {1} points per game", i + 1, Math.Round(playersPoints[i], 2) );
             }
             Console.ReadKey();
+        }
+
+        public static List<Card> GenerateDeck()
+        {
+            List<Card> cards = new List<Card>();
+            for (int i = 0; i < Enum.GetNames(typeof(Color)).Length; i++)
+            {
+                for (int j = 0; j < Enum.GetNames(typeof(Figure)).Length; j++)
+                {
+                    cards.Add(new Card((Figure)j, (Color)i));
+                }
+            }
+            return cards;
+        }
+
+        public static int CalculatePoints(List<Card> myCards)
+        {
+            int points = 0;
+            int[] colors = { 0, 0, 0, 0 };
+            for (int i = 0; i < myCards.Count; i++)
+            {
+                switch (myCards[i].Figure)
+                {
+                    case Figure.Ace:
+                        points += 4;
+                        break;
+                    case Figure.King:
+                        points += 3;
+                        break;
+                    case Figure.Queen:
+                        points += 2;
+                        break;
+                    case Figure.Jack:
+                        points += 1;
+                        break;
+                    default:
+                        break;
+                }
+
+                switch (myCards[i].Color)
+                {
+                    case Color.Clubs:
+                        colors[0]++;
+                        break;
+                    case Color.Diamonds:
+                        colors[1]++;
+                        break;
+                    case Color.Hearts:
+                        colors[2]++;
+                        break;
+                    case Color.Spades:
+                        colors[3]++;
+                        break;
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (colors[i] == 0)
+                    points += 4;
+            }
+            return points;
         }
     }
 }
--- commons/UnityBridge/UnityBridge/CardShuffler.cs
+++ commons/UnityBridge/UnityBridge/CardShuffler.cs
@@ -3,32 +3,21 @@ using System.Collections.Generic;
 
 namespace UnityBridge
 {
-    public class CardShuffler
+    public class CardShuffler<T>
     {
         private Random randomizer;
+        private List<T> deck;
 
-        public CardShuffler()
+        public CardShuffler(List<T> deck)
         {
             randomizer = new Random();
+            this.deck = deck;
         }
 
-        public List<Card> GenerateDeck()
+        public List<T> Shuffle()
         {
-            List<Card> cards = new List<Card>();
-            for(int i = 0; i < Enum.GetNames(typeof(Color)).Length; i++)
-            {
-                for(int j = 0; j < Enum.GetNames(typeof(Figure)).Length; j++)
-                {
-                    cards.Add( new Card( (Figure)j, (Color)i ) );
-                }
-            }
-            return cards;
-        }
-
-        public List<Card> Shuffle(List<Card> deck)
-        {
-            List<Card> leftHandCards = new List<Card>();
-            List<Card> rightHandCards = new List<Card>();
+            List<T> leftHandCards = new List<T>();
+            List<T> rightHandCards = new List<T>();
 
             int[] shuffleTimes = { 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
             int numberOfShuffles = randomizer.Next(0, shuffleTimes.Length);
@@ -80,7 +69,7 @@ namespace UnityBridge
             return deck;
         }
 
-        private double CalculateProbability(List<Card> hand, int wholeDeckSize)
+        private double CalculateProbability(List<T> hand, int wholeDeckSize)
         {
             if (hand.Count == 0)
                 return 0;
@@ -89,13 +78,13 @@ namespace UnityBridge
             return probability;
         }
 
-        public List<List<Card>> GiveCardsToPlayers(List<Card> shuffledDeck)
+        public List<List<T>> GiveCardsToPlayers(List<T> shuffledDeck)
         {
-            List<List<Card>> playersDecks = new List<List<Card>>();
-            playersDecks.Add(new List<Card>());
-            playersDecks.Add(new List<Card>());
-            playersDecks.Add(new List<Card>());
-            playersDecks.Add(new List<Card>());
+            List<List<T>> playersDecks = new List<List<T>>();
+            playersDecks.Add(new List<T>());
+            playersDecks.Add(new List<T>());
+            playersDecks.Add(new List<T>());
+            playersDecks.Add(new List<T>());
 
             int index;
             for (int i = 0; i < shuffledDeck.Count; i++)
@@ -104,55 +93,6 @@ namespace UnityBridge
                 playersDecks[index].Add(shuffledDeck[i]);
             }
             return playersDecks;
-        }
-
-        public int CalculatePoints(List<Card> myCards)
-        {
-            int points = 0;
-            int[] colors = { 0, 0, 0, 0 };
-            for(int i = 0; i < myCards.Count; i++)
-            {
-                switch (myCards[i].Figure)
-                {
-                    case Figure.Ace:
-                        points += 4;
-                        break;
-                    case Figure.King:
-                        points += 3;
-                        break;
-                    case Figure.Queen:
-                        points += 2;
-                        break;
-                    case Figure.Jack:
-                        points += 1;
-                        break;
-                    default:
-                        break;
-                }
-
-                switch (myCards[i].Color)
-                {
-                    case Color.Clubs:
-                        colors[0]++;
-                        break;
-                    case Color.Diamonds:
-                        colors[1]++;
-                        break;
-                    case Color.Hearts:
-                        colors[2]++;
-                        break;
-                    case Color.Spades:
-                        colors[3]++;
-                        break;
-                }
-            }
-
-            for (int i = 0; i < 4; i++)
-            {
-                if (colors[i] == 0)
-                    points += 4;
-            }
-            return points;
         }
     }
 }
