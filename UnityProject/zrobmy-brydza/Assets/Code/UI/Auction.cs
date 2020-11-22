31cf1968176d349979b4b9a7de30fceda72b90a8
niedzwir <r_adekn@wp.pl>
2020-11-22 20:12:35 +0100

    Auction.cs

--- UnityProject/zrobmy-brydza/Assets/Code/UI/Auction.cs
+++ UnityProject/zrobmy-brydza/Assets/Code/UI/Auction.cs
@@ -6,15 +6,20 @@ public class Auction : MonoBehaviour
 {
     public class Bet
     {
-        public int Number; //1-7
-        public int Suit; //1-5
+        public int Number = 0; //1-7
+        public int Suit = 0; //1-5
         public int Player; //1-4
     }
-    
-    private Bet HighestBet;
 
+    void Start() {
+        this.HighestBet = new Bet();
+        this.CurrentBet = new Bet();
+    }
+
+    private Bet HighestBet;
+    private Bet CurrentBet;
     public bool MakeBet(Bet bet) {
-        if (bet.Number > HighestBet.Number || (bet.Number == HighestBet.Number && bet.Suit > HighestBet.Suit)) {
+        if (bet.Suit > 0 && bet.Number > 0 &&(bet.Number > HighestBet.Number || (bet.Number == HighestBet.Number && bet.Suit > HighestBet.Suit))) {
             HighestBet.Number = bet.Number;
             HighestBet.Suit = bet.Suit;
             HighestBet.Player = bet.Player;
@@ -22,7 +27,69 @@ public class Auction : MonoBehaviour
         }
         else
         {
+            if (bet.Number == -1) {
+                return true;
+            }
             return false;
         }
+    }
+
+    public void one(){
+        this.CurrentBet.Number = 1;
+    }
+    public void two()
+    {
+        this.CurrentBet.Number = 2;
+    }
+    public void three()
+    {
+        this.CurrentBet.Number = 3;
+    }
+    public void four()
+    {
+        this.CurrentBet.Number = 4;
+    }
+    public void five()
+    {
+        this.CurrentBet.Number = 5;
+    }
+    public void six()
+    {
+        this.CurrentBet.Number = 6;
+    }
+    public void seven()
+    {
+        this.CurrentBet.Number = 7;
+    }
+    public void C()
+    {
+        this.CurrentBet.Suit = 1;
+    }
+    public void D()
+    {
+        this.CurrentBet.Suit = 2;
+    }
+    public void H()
+    {
+        this.CurrentBet.Suit = 3;
+    }
+    public void S()
+    {
+        this.CurrentBet.Suit = 4;
+    }
+    public void NT()
+    {
+        this.CurrentBet.Suit = 5;
+    }
+    public void pas(){
+        this.CurrentBet.Number = -1;
+    }
+    public void ok() {
+        MakeBet(CurrentBet);
+        // if(b){zmiengracza()}
+        CurrentBet.Number = 0;
+        CurrentBet.Suit = 0;
+        Debug.Log(HighestBet.Number);
+        Debug.Log(HighestBet.Suit);
     }
 }
