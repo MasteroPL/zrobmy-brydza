79f9108036f4a227b1f0968db601a74c4297539a
Greenhoody <1mk2mk3mk@gmail.com>
2020-11-26 18:56:57 +0100

    Connection UI

---
+++ UnityProject/zrobmy-brydza/Assets/Code/UI/TMPInputFieldToAsterix.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TMPInputFieldToAsterix : MonoBehaviour
+{
+    // pass to textToChange Input field to be replays with asterixes
+    public TMP_InputField textToChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textToChange.contentType = TMP_InputField.ContentType.Password;
+    }
+}
