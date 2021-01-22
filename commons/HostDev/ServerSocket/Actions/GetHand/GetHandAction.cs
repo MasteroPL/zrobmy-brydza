bd373dffe533ed30b628568a310518b1fe4f02d6
Greenhoody <1mk2mk3mk@gmail.com>
2021-01-22 09:57:40 +0100

    Akcje

---
+++ commons/HostDev/ServerSocket/Actions/GetHand/GetHandAction.cs
@@ -0,0 +1,53 @@
+﻿using EasyHosting.Models.Actions;
+using EasyHosting.Models.Serialization;
+using EasyHosting.Models.Server;
+using ServerSocket.Models;
+using ServerSocket.Serializers;
+using GameManagerLib.Exceptions;
+using GameManagerLib.Models;
+
+namespace ServerSocket.Actions.GetHand
+{
+    class GetHandAction : BaseAction
+    {
+    public GetHandAction() : base(
+        typeof(RequestSerializer),
+        typeof(ResponseSerializer)
+        ){ }
+        protected override BaseSerializer PerformAction(ClientConnection conn, BaseSerializer requestData)
+        {
+            RequestSerializer data = (RequestSerializer)requestData;
+            ResponseSerializer resp = (ResponseSerializer)InitializeResponseSerializer();
+            resp.Status = "OK";
+
+            Lobby lobby = (Lobby)conn.Session.Get("joined-lobby");
+            Match game = lobby.Game;
+
+            Player player;
+
+            CardSerializer[] cards = data.Hand;
+            Card[] hand = new Card[13];
+
+            for (int i = 0; i < hand.Length; i++)
+            {
+                hand[i] = new Card((Card.CardFigure)cards[i].Figure, (Card.CardColor)cards[i].Color, (Card.PlayerTag)data.PlayerID, (Card.CardState)cards[i].State);
+            }
+
+            Player player;
+
+            foreach (player in game.PlayerList)
+            {
+                if(player.Tag == (Player.PlayerTag)data.PlayerID)
+                {
+                    break;
+                }
+            }
+
+            try
+            {
+                player.Hand = hand;
+            }
+
+            return resp;
+        }
+}
