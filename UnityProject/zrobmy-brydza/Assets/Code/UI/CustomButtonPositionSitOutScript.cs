f28b8adc199a2afcd801be7a466c6da14c8b26a7
ElAdriano <adrian.ukryte@gmail.com>
2021-02-09 19:50:36 +0100

    missing files

---
+++ UnityProject/zrobmy-brydza/Assets/Code/UI/CustomButtonPositionSitOutScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace Assets.Code.UI
+{
+    class CustomButtonPositionSitOutScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    {
+        [SerializeField] Button ReferencedButton;
+        [SerializeField] GameManagerScript GameManager;
+
+        public void OnPointerEnter(PointerEventData data)
+        {
+            bool available = GameManager.CheckSeatAvailability(ReferencedButton.gameObject.name[0]);
+            if (!available)
+            {
+                ReferencedButton.image.color = new Color32(255, 221, 0, 255);
+            }
+        }
+
+        public void OnPointerExit(PointerEventData data)
+        {
+            ReferencedButton.image.color = new Color32(255, 255, 255, 0);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            bool available = GameManager.CheckSeatAvailability(ReferencedButton.gameObject.name[0]);
+            if (!available)
+            {
+                GameManager.SitOutPlayer(ReferencedButton.gameObject.name[0]);
+            }
+        }
+    }
+}
