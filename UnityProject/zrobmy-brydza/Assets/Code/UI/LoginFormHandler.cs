90ff5c521300c3dea32ab76baa39686ab1137675
ElAdriano <adrian.ukryte@gmail.com>
2021-04-05 22:12:11 +0200

    Dodanie ekranu logowania + wystawienie miejsca na podpięcie pod serwer

---
+++ UnityProject/zrobmy-brydza/Assets/Code/UI/LoginFormHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Assets.Code.UI;
+using System.Text.RegularExpressions;
+using System.Text;
+
+public class LoginFormHandler : MonoBehaviour
+{
+    private Regex PasswordRegexValidator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PasswordRegexValidator = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$");
+
+        GameObject.Find("LoginButton").GetComponent<Button>().onClick.AddListener(() => { LoginClickHandler(); });
+        GameObject.Find("ComeBackButton").GetComponent<Button>().onClick.AddListener(() => { ComeBackClickHandler(); });
+
+        GameObject.Find("PasswordInput").GetComponent<InputField>().onValueChanged.AddListener((value) => { PasswordValidation(value); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoginClickHandler()
+    {
+        Debug.Log("Login requests has to be sent and handled");
+        UserData.LoggedIn = true;
+
+        Debug.Log("Login: " + GameObject.Find("LoginInput").GetComponent<InputField>().text);
+        Debug.Log("Haslo: " + GameObject.Find("PasswordInput").GetComponent<InputField>().text);
+
+        SceneManager.LoadScene(0);
+    }
+
+    void ComeBackClickHandler()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void PasswordValidation(string PasswordInputValue)
+    {
+        if (!PasswordRegexValidator.IsMatch(PasswordInputValue) || PasswordInputValue.Length >= 32)
+        {
+            GameObject.Find("PasswordInput/Text").GetComponent<Text>().color = Color.red;
+        }
+        else
+        {
+            GameObject.Find("PasswordInput/Text").GetComponent<Text>().color = Color.black;
+        }
+    }
+}
