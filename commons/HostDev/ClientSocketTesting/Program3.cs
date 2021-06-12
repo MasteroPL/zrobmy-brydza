b72285e78e63a8e747240d4bad2b39255065e485
Marcin <marcinbaranowski8899@gmail.com>
2021-06-12 18:48:23 +0200

    wstępne dodanie AI

---
+++ commons/HostDev/ClientSocketTesting/Program3.cs
@@ -0,0 +1,143 @@
+﻿using EasyHosting.Models.Actions;
+using EasyHosting.Models.Client;
+using EasyHosting.Models.Client.Serializers;
+using GameManagerLib.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace ClientSocketTesting
+{
+    class Program3
+	{
+		static void OnRequestResponseMacius(object sender, Request request)
+		{
+			Console.WriteLine("[Macius]>" + request.ResponseData);
+		}
+		static void OnSignalMacius(object sender, StandardResponseWrapperSerializer signal)
+		{
+			Console.WriteLine("[Macius]>" + signal.Data);
+		}
+		static void OnRequestResponsePawelek(object sender, Request request)
+		{
+			Console.WriteLine("[Pawelek]>" + request.ResponseData);
+		}
+		static void OnSignalPawelek(object sender, StandardResponseWrapperSerializer signal)
+		{
+			Console.WriteLine("[Pawelek]>" + signal.Data);
+		}
+		static void OnRequestResponseMarcin(object sender, Request request)
+		{
+			Console.WriteLine("[Marcin]>" + request.ResponseData);
+		}
+		static void OnSignalMarcin(object sender, StandardResponseWrapperSerializer signal)
+		{
+			Console.WriteLine("[Marcin]>" + signal.Data);
+		}
+		static void OnRequestResponseMarcin2_0(object sender, Request request)
+		{
+			Console.WriteLine("[Marcin2_0]>" + request.ResponseData);
+		}
+		static void OnSignalMarcin2_0(object sender, StandardResponseWrapperSerializer signal)
+		{
+			Console.WriteLine("[Marcin2_0]>" + signal.Data);
+		}
+
+		public static ActionsSerializer WrapRequestData(string actionName, JObject data)
+		{
+			var result = new ActionsSerializer();
+			result.Actions = new ActionSerializer[1];
+			var tmp = new ActionSerializer();
+
+			tmp.ActionName = actionName;
+			tmp.ActionData = data;
+
+			result.Actions[0] = tmp;
+
+			return result;
+		}
+
+		public static void Main2(string[] args)
+		{
+			ClientSocket MaciusSocket = null, PawelekSocket = null, MarcinSocket = null, Marcin2_0Socket;
+
+			MaciusSocket = new ClientSocket("127.0.0.1");
+			PawelekSocket = new ClientSocket("127.0.0.1");
+			MarcinSocket = new ClientSocket("127.0.0.1");
+			Marcin2_0Socket = new ClientSocket("127.0.0.1");
+
+			MaciusSocket.RequestResponseReceived += OnRequestResponseMacius;
+			MaciusSocket.SignalReceived += OnSignalMacius;
+			PawelekSocket.RequestResponseReceived += OnRequestResponsePawelek;
+			PawelekSocket.SignalReceived += OnSignalPawelek;
+			MarcinSocket.RequestResponseReceived += OnRequestResponseMarcin;
+			MarcinSocket.SignalReceived += OnSignalMarcin;
+			Marcin2_0Socket.RequestResponseReceived += OnRequestResponseMarcin2_0;
+			Marcin2_0Socket.SignalReceived += OnSignalMarcin2_0;
+
+			DumbAI MaciusAI = new DumbAI()
+			{
+				ClientSocket = MaciusSocket,
+				Position = PlayerTag.N,
+				Username = "Macius"
+			};
+			DumbAI PawelekAI = new DumbAI()
+			{
+				ClientSocket = PawelekSocket,
+				Position = PlayerTag.W,
+				Username = "Pawelek"
+			};
+			DumbAI MarcinAI = new DumbAI() {
+			    ClientSocket = MarcinSocket,
+			    Position = PlayerTag.E,
+			    Username = "Marcin"
+			};
+			DumbAI Marcin2_0AI = new DumbAIMarcin2_0()
+			{
+				ClientSocket = Marcin2_0Socket,
+				Position = PlayerTag.S,
+				Username = "The_real_marcin"
+			};
+
+			MaciusAI.Init();
+			PawelekAI.Init();
+			MarcinAI.Init();
+			Marcin2_0AI.Init();
+
+			Marcin2_0AI.Authorize();
+			MaciusAI.Authorize();
+			PawelekAI.Authorize();
+			MarcinAI.Authorize();
+
+			MaciusAI.LoadGame();
+			PawelekAI.LoadGame();
+			MarcinAI.LoadGame();
+			Marcin2_0AI.LoadGame();
+
+			MaciusAI.Sit();
+			PawelekAI.Sit();
+			MarcinAI.Sit();
+			Marcin2_0AI.Sit();
+
+			Console.WriteLine("> Inicjalne ładowanie zakończone");
+
+			while (true)
+			{
+				MaciusAI.Play();
+				Thread.Sleep(500);
+				PawelekAI.Play();
+				Thread.Sleep(500);
+				MarcinAI.Play();
+				Thread.Sleep(500);
+				Marcin2_0AI.Play();
+				Thread.Sleep(500);
+			}
+		}
+	}
+}
---
+++ commons/HostDev/ClientSocketTesting/UserAI.cs
@@ -0,0 +1,471 @@
+﻿using EasyHosting.Models;
+using EasyHosting.Models.Actions;
+using EasyHosting.Models.Client;
+using GameManagerLib.Models;
+using Newtonsoft.Json.Linq;
+using ServerSocket.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GetTableInfoActionRequestSerializer = ServerSocket.Actions.GetTableInfo.RequestSerializer;
+using GetTableInfoActionResponseSerializer = ServerSocket.Actions.GetTableInfo.ResponseSerializer;
+using GetHandActionRequestSerializer = ServerSocket.Actions.GetHand.RequestSerializer;
+using GetHandActionResponseSerializer = ServerSocket.Actions.GetHand.ResponseSerializer;
+using SitActionRequestSerializer = ServerSocket.Actions.Sit.RequestSerializer;
+using SitActionResponseSerializer = ServerSocket.Actions.Sit.ResponseSerializer;
+using BidActionRequestSerializer = ServerSocket.Actions.Bid.RequestSerializer;
+using BidActionResponseSerializer = ServerSocket.Actions.Bid.ResponseSerializer;
+using PutCardActionRequestSerializer = ServerSocket.Actions.PutCard.RequestSerializer;
+using PutCardActionResponseSerializer = ServerSocket.Actions.PutCard.ResponseSerializer;
+using EasyHosting.Models.Client.Serializers;
+using GameManagerLib.Exceptions;
+
+namespace ClientSocketTesting
+{
+    public class UserAI
+    {
+        public ClientSocket ClientSocket = null;
+        public Match Game = null;
+        public string Username;
+        public PlayerTag Position;
+        public bool SendGetHandRequest = false;
+        public AI AILogic = null;
+
+
+        public void Init()
+        {
+            ClientSocket.SignalReceived += OnServerSignalReceive;
+        }
+        ~UserAI()
+        {
+            ClientSocket.SignalReceived -= OnServerSignalReceive;
+        }
+
+        public static ActionsSerializer WrapRequestData(string actionName, JObject data)
+        {
+            var result = new ActionsSerializer();
+            result.Actions = new ActionSerializer[1];
+            var tmp = new ActionSerializer();
+
+            tmp.ActionName = actionName;
+            tmp.ActionData = data;
+
+            result.Actions[0] = tmp;
+
+            return result;
+        }
+
+        public void PerformServerAction(string actionName, JObject data, Action<Request, ActionsSerializer, object> callback = null, object additionalData = null)
+        {
+            var requestData = WrapRequestData(actionName, data);
+            var currentRequest = ClientSocket.SendRequest(requestData.GetApiObject());
+
+            while (currentRequest.RequestState != RequestState.RESPONSE_RECEIVED)
+            {
+                ClientSocket.UpdateCommunication();
+            }
+
+            var actsSer = new ActionsSerializer(currentRequest.ResponseData);
+            actsSer.Validate();
+
+            callback?.Invoke(currentRequest, actsSer, additionalData);
+        }
+        private void HandleLobbySignal(JObject signalData)
+        {
+            var signalName = (string)signalData.GetValue("signal");
+            // start gry
+            if (signalName == PlayerClickedGameStartSerializer.SIGNAL_PLAYER_READY)
+            {
+                var serializer = new PlayerClickedGameStartSerializer(signalData);
+                serializer.Validate();
+
+                Game.Start();
+                SendGetHandRequest = true;
+            }
+            else if (signalName == LobbySignalNewBidSerializer.SIGNAL_NEW_BID)
+            {
+                var serializer = new LobbySignalNewBidSerializer(signalData);
+                serializer.Validate();
+
+                try
+                {
+                    var contract = new Contract(
+                        (ContractHeight)serializer.Height,
+                        (ContractColor)serializer.Color,
+                        (PlayerTag)serializer.PlaceTag,
+                        serializer.X,
+                        serializer.XX
+                    );
+                    Game.AddBid(contract);
+                }
+                catch (WrongBidException)
+                {
+                    // TODO: Pobrać całą licytację jeszcze raz
+                }
+            }
+            // Użytkownik usiadł na wybranym miejscu
+            else if (signalName == LobbySignalUserSatSerializer.SIGNAL_USER_SAT)
+            {
+                var serializer = new LobbySignalUserSatSerializer(signalData);
+                serializer.Validate();
+
+                if (serializer.Username != Username)
+                {
+                    Game.AddPlayer(new Player((PlayerTag)serializer.PlaceTag, serializer.Username));
+                }
+            }
+            else if (signalName == LobbySignalUserSittedOutSerializer.SIGNAL_USER_SITTED_OUT)
+            {
+                var serializer = new LobbySignalUserSittedOutSerializer(signalData);
+                serializer.Validate();
+
+                var player = Game.GetPlayerAt((PlayerTag)serializer.PlaceTag);
+                Game.RemovePlayer(player);
+            }
+            else if (signalName == PutCardSignalSerializer.SIGNAL_USER_PUT_CARD)
+            {
+                var serializer = new PutCardSignalSerializer(signalData);
+                serializer.Validate();
+
+                if (serializer.Username != Username)
+                {
+                    Game.CurrentGame.NextCard(new Card((CardFigure)serializer.CardFigure, (CardColor)serializer.CardColor, (PlayerTag)serializer.OwnerPosition));
+                }
+            }
+            // Następna licytacja
+            else if (signalName == LobbySignalGameStartedNextBiddingSerializer.SIGNAL_GAME_STARTED_NEXT_BIDDING)
+            {
+                var serializer = new LobbySignalGameStartedNextBiddingSerializer(signalData);
+
+                Game.ClearPlayerHands();
+
+                AILogic = null;
+
+                if (Game.GameState != GameState.BIDDING)
+                {
+                    Game.GameState = GameState.BIDDING;
+                    Game.StartBidding();
+                }
+            }
+            else if (signalName == LobbySignalGameFinishedSerializer.SIGNAL_GAME_FINISHED)
+            {
+                var serializer = new LobbySignalGameFinishedSerializer(signalData);
+
+                Game.ClearPlayerHands();
+  
+                Game.GameState = GameState.GAME_FINISHED;
+            }
+        }
+        private void OnServerSignalReceive(object sender, StandardResponseWrapperSerializer data)
+        {
+            switch (data.CommunicateType)
+            {
+                case "LOBBY_SIGNAL":
+                    HandleLobbySignal(data.Data);
+                    break;
+
+                default:
+                    //Debug.Log("Unrecognized signal");
+                    break;
+            }
+        }
+
+        public void Authorize()
+        {
+            var authData = new AuthorizationSerializer()
+            {
+                Login = Username,
+                LobbyPassword = "",
+                LobbyId = "DEFAULT"
+            };
+            var authRequest = ClientSocket.SendRequest(authData.GetApiObject());
+            while (authRequest.RequestState != RequestState.RESPONSE_RECEIVED)
+            {
+                ClientSocket.UpdateCommunication();
+            }
+            Console.WriteLine("Authorized " + Username);
+        }
+
+        public void LoadGame()
+        {
+            PerformServerAction("get-table-info", null, LoadGameCallback, null);
+        }
+        protected void LoadGameCallback(Request request, ActionsSerializer response, object additionalData)
+        {
+            Game = new Match(enableCardsShufflingAndDistributing: false);
+
+            AILogic = null;
+
+            var rs = new GetTableInfoActionResponseSerializer(response.Actions[0].ActionData);
+            rs.Validate();
+
+            Game.RoundsNS = rs.RoundsNS;
+            Game.RoundsWE = rs.RoundsWE;
+            Game.PointsNS[0] = rs.PointsNSBelowLine;
+            Game.PointsNS[1] = rs.PointsNSAboveLine;
+            Game.PointsWE[0] = rs.PointsWEBelowLine;
+            Game.PointsWE[1] = rs.PointsWEAboveLine;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (rs.Players[i] != null)
+                {
+                    Game.AddPlayer(new Player((PlayerTag)rs.Players[i].PlayerTag, rs.Players[i].Username));
+                }
+            }
+
+            if (rs.CurrentBidding != null && rs.CurrentBidding.ContractList != null)
+            {
+                foreach (var contract in rs.CurrentBidding.ContractList)
+                {
+                    if (contract != null)
+                    {
+                        Game.AddBid(new Contract(
+                            (ContractHeight)contract.ContractHeight,
+                            (ContractColor)contract.ContractColor,
+                            (PlayerTag)contract.PlayerTag,
+                            contract.XEnabled,
+                            contract.XXEnabled
+                        ));
+                    }
+                }
+            }
+        }
+
+        public void Sit()
+        {
+            var sitRequestData = new SitActionRequestSerializer()
+            {
+                PlaceTag = (int)Position
+            };
+            PerformServerAction("sit", sitRequestData.GetApiObject(), null, null);
+            Game.AddPlayer(new Player(Position, Username));
+        }
+
+
+        public void GetHand()
+        {
+            var getHandRequestData = new GetHandActionRequestSerializer()
+            {
+                PlayerTag = (int)Position
+            };
+            PerformServerAction("get-hand", getHandRequestData.GetApiObject(), GetHandCallback);
+        }
+        protected void GetHandCallback(Request request, ActionsSerializer response, object additionalData)
+        {
+            SendGetHandRequest = false;
+            var data = new GetHandActionResponseSerializer(response.Actions[0].ActionData);
+            data.Validate();
+
+            int index = 0;
+            Card tmp;
+            var player = Game.GetPlayerAt(Position);
+
+            List<int> C = new List<int>();
+            List<int> D = new List<int>();
+            List<int> H = new List<int>();
+            List<int> S = new List<int>();
+
+            foreach (var card in data.Cards)
+            {
+                tmp = new Card(
+                    (CardFigure)card.Figure,
+                    (CardColor)card.Color,
+                    Position,
+                    (CardState)card.State
+                );
+                
+                player.Hand[index] = tmp;
+                index++;
+
+                if(card.Color + 1 == 1)
+                {
+                    C.Add(card.Figure);
+                }
+
+                if (card.Color + 1 == 2)
+                {
+                    D.Add(card.Figure);
+                }
+
+                if (card.Color + 1 == 3)
+                {
+                    H.Add(card.Figure);
+                }
+
+                if (card.Color + 1 == 4)
+                {
+                    S.Add(card.Figure);
+                }
+
+                AILogic.AI_hand = new AI.Hand(C, D, H, S);
+            }
+        }
+
+        public void GetGrandpaHand()
+        {
+            var getHandRequestData = new GetHandActionRequestSerializer()
+            {
+                PlayerTag = ((int)Position + 2) % 4
+            };
+            PerformServerAction("get-hand", getHandRequestData.GetApiObject(), GetGrandpaHandCallback);
+        }
+        protected void GetGrandpaHandCallback(Request request, ActionsSerializer response, object additionalData)
+        {
+            var data = new GetHandActionResponseSerializer(response.Actions[0].ActionData);
+            data.Validate();
+           
+
+            int index = 0;
+            Card tmp;
+            var player = Game.GetPlayerAt((PlayerTag)(((int)Position + 2) % 4));
+
+            List<int> C = new List<int>();
+            List<int> D = new List<int>();
+            List<int> H = new List<int>();
+            List<int> S = new List<int>();
+
+            foreach (var card in data.Cards)
+            {
+                tmp = new Card(
+                    (CardFigure)card.Figure,
+                    (CardColor)card.Color,
+                    (PlayerTag)(((int)Position + 2) % 4),
+                    (CardState)card.State
+                );
+                if (card.Color + 1 == 1)
+                {
+                    C.Add(card.Figure);
+                }
+
+                if (card.Color + 1 == 2)
+                {
+                    D.Add(card.Figure);
+                }
+
+                if (card.Color + 1 == 3)
+                {
+                    H.Add(card.Figure);
+                }
+
+                if (card.Color + 1 == 4)
+                {
+                    S.Add(card.Figure);
+                }
+                player.Hand[index] = tmp;
+                index++;
+            }
+
+            AILogic.Grandpa_hand = new AI.Hand(C, D, H, S);
+        }
+
+
+        public virtual void Play()
+        {
+            ClientSocket.UpdateCommunication();
+
+            if (SendGetHandRequest)
+            {
+                GetHand();
+            }
+
+            switch (Game.GameState)
+            {
+                case GameState.BIDDING:
+                    PlayBidding();
+                    break;
+                case GameState.PLAYING:
+                    PlayPlaying();
+                    break;
+            }
+        }
+        protected virtual void PlayBidding()
+        {
+            if (Game.CurrentBidding.CurrentPlayer == Position)
+            {
+                var history = Game.CurrentBidding.ContractList;
+
+                List<int> AI_history = new List<int>();
+
+                foreach(var bid in history)
+                {
+                    int AI_bid = ((int)(bid.ContractColor) + 1) + (int)(bid.ContractHeight) * 10;
+                    AI_history.Add(AI_bid);
+                }
+
+                AILogic.Bid(AI_history);
+
+                var data = new BidActionRequestSerializer()
+                {
+                    Height = (int)ContractHeight.NONE,
+                    Color = (int)ContractColor.NONE,
+                    X = false,
+                    XX = false
+                };
+
+                Console.WriteLine(Username + "> Licytuje: pas");
+                PerformServerAction("bid", data.GetApiObject(), null, null);
+            }
+        }
+        protected virtual void PlayPlaying()
+        {
+
+            if (
+                ((int)Game.CurrentBidding.Declarer + 2) % 4 != (int)Position
+                && Game.CurrentGame.CurrentPlayer == Position)
+            {
+                for (int i = 0; i < MyCards.Count; i++)
+                {
+                    if (Game.CheckNextCard(
+                        Position,
+                        MyCards[i].Color,
+                        MyCards[i].Figure
+                    ))
+                    {
+                        var data = new PutCardActionRequestSerializer()
+                        {
+                            CardOwnerPosition = (int)Position,
+                            Color = (int)MyCards[i].Color,
+                            Figure = (int)MyCards[i].Figure
+                        };
+
+                        PerformServerAction("put-card", data.GetApiObject(), PutCardCallback, MyCards[i]);
+                        break;
+                    }
+                }
+            }
+            else if (
+                Game.CurrentBidding.Declarer == Position
+                && ((int)Game.CurrentGame.CurrentPlayer + 2) % 4 == (int)Position
+            )
+            {
+                for (int i = 0; GrandpaCards != null && i < GrandpaCards.Count; i++)
+                {
+                    if (Game.CheckNextCard(
+                        (PlayerTag)(((int)Position + 2) % 4),
+                        GrandpaCards[i].Color,
+                        GrandpaCards[i].Figure
+                    ))
+                    {
+                        var data = new PutCardActionRequestSerializer()
+                        {
+                            CardOwnerPosition = ((int)Position + 2) % 4,
+                            Color = (int)GrandpaCards[i].Color,
+                            Figure = (int)GrandpaCards[i].Figure
+                        };
+
+                        PerformServerAction("put-card", data.GetApiObject(), PutCardCallback, GrandpaCards[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        protected void PutCardCallback(Request request, ActionsSerializer response, object additionalData)
+        {
+            var card = (Card)additionalData;
+            Game.CurrentGame.NextCard(card);
+        }
+    }
+}
